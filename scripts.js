0ad7874dd2261c47fe2f4dc3d1fd3a552d2948d6
SanaShabeer <156203074+SanaShabeer@users.noreply.github.com>
2024-09-04 11:57:04 +0530

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -2,5 +2,5 @@
 
 document.getElementById('start-interview-btn').addEventListener('click', function() {
     alert('The interview has started. Please follow the prompts.');
-    // Add code here to start the chatbot or redirect to the chatbot page
+    // You can add code here to start the chatbot or redirect to the chatbot page
 });
