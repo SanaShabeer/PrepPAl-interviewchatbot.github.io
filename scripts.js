8992fbec85f87caa0c6d5c51f606a808a927af63
SanaShabeer <156203074+SanaShabeer@users.noreply.github.com>
2024-09-04 12:26:23 +0530

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
 function startChat() {
-    document.getElementById('chatbot-instructions').innerText = "The chatbot will now ask you a series of interview questions. Please answer them to the best of your ability.";
-  }
-  
+    document.getElementById('chatbot-instructions').innerHTML = "The chatbot will ask you a series of questions. Answer them to the best of your ability!";
+}
+
