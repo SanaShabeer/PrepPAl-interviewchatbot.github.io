a91d880589d04c373190f46d920751fe121256b4
SurbhiMKumar <156202997+SurbhiMKumar@users.noreply.github.com>
2024-09-06 18:45:28 +0530

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
 function startChat() {
-    document.getElementById('chatbot-instructions').innerHTML = "The chatbot will ask you a series of questions. Answer them to the best of your ability!";
+    window.location.href = "login.html";
 }
-
